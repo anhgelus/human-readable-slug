c36c9283a218dd2947070725a6936c096cbb5048
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2023-10-04 12:37:39 +0200

    test(options): add test for custom options

--- slug_test.go
+++ slug_test.go
@@ -1,6 +1,7 @@
 package human_readable_slug
 
 import (
+	"strings"
 	"testing"
 	"time"
 )
@@ -10,6 +11,60 @@ func TestGenerateSlug(t *testing.T) {
 		slug := GenerateSlug(time.Now().Unix(), 7)
 		t.Log(slug)
 		if !IsHumanReadable(slug) {
+			t.Error("slug", slug, "is not human readable")
+		}
+	}
+}
+
+func TestNoNumbersSlug(t *testing.T) {
+	options := CustomSlug{
+		HasCapitalLetters:        true,
+		HasNumbers:               false,
+		CanBe2ConsecutiveLetters: false,
+	}
+	for i := 0; i < 10; i++ {
+		slug := options.GenerateSlug(time.Now().Unix(), 7)
+		t.Log(slug)
+		if strings.ContainsAny(slug, numbers) {
+			t.Error("slug", slug, "contains number")
+		}
+		if !options.IsHumanReadable(slug) {
+			t.Error("slug", slug, "is not human readable")
+		}
+	}
+}
+
+func TestNoUpperSlug(t *testing.T) {
+	options := CustomSlug{
+		HasCapitalLetters:        false,
+		HasNumbers:               true,
+		CanBe2ConsecutiveLetters: false,
+	}
+	for i := 0; i < 10; i++ {
+		slug := options.GenerateSlug(time.Now().Unix(), 7)
+		t.Log(slug)
+		if strings.ContainsAny(slug, strings.ToUpper(minLetters)) {
+			t.Error("slug", slug, "contains upper case")
+		}
+		if !options.IsHumanReadable(slug) {
+			t.Error("slug", slug, "is not human readable")
+		}
+	}
+}
+
+func TestNoUpperNoNumbersSlug(t *testing.T) {
+	options := CustomSlug{
+		HasCapitalLetters:        false,
+		HasNumbers:               false,
+		CanBe2ConsecutiveLetters: false,
+	}
+	for i := 0; i < 10; i++ {
+		slug := options.GenerateSlug(time.Now().Unix(), 7)
+		t.Log(slug)
+		if strings.ContainsAny(slug, strings.ToUpper(minLetters)+numbers) {
+			t.Error("slug", slug, "contains upper case")
+		}
+		if !options.IsHumanReadable(slug) {
 			t.Error("slug", slug, "is not human readable")
 		}
 	}
